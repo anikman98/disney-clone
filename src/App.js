5a2435be15ab5ccc0f45e4754406a54f886e7783
anikman <anikman98@gmail.com>
2022-09-21 02:42:20 +0530

    Added login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello Youtube!</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
