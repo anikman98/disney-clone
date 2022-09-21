dd27314ff910f71f5814c411114c0fd64d7e26d7
anikman <anikman98@gmail.com>
2022-09-22 01:17:31 +0530

    completed header menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,49 @@
 import styled from 'styled-components';
+import  {auth,provider} from '../firebase';
 
 const header = () => {
+
+    const handleAuth = () => {
+        auth.signInWithPopup(provider).then((result) => {
+            console.log(result);
+        }).catch(error => {
+            alert(error.message);
+        })
+    };
+
+
   return (
     <Nav>
-    <Logo>
-        <img src="/resources/images/logo.svg" alt="Disney" />
-    </Logo>
-    <NavMenu>
-        <a href="/home">
-            <img src="/resources/images/home-icon.svg" alt="HOME" />
-        </a>
-        <span>Home</span>
-    </NavMenu>
+        <Logo>
+            <img src="/resources/images/logo.svg" alt="Disney" />
+        </Logo>
+        <NavMenu>
+            <a href="/home">
+                <img src="/resources/images/home-icon.svg" alt="HOME" />
+                <span>HOME</span>
+            </a>
+            <a>
+                <img src="/resources/images/search-icon.svg" alt="SEARCH" />
+                <span>SEARCH</span>
+            </a>
+            <a>
+                <img src="/resources/images/watchlist-icon.svg" alt="WATCHLIST" />
+                <span>WATCHLIST</span>
+            </a>
+            <a>
+                <img src="/resources/images/original-icon.svg" alt="ORIGINALS" />
+                <span>ORIGINALS</span>
+            </a>
+            <a>
+                <img src="/resources/images/movie-icon.svg" alt="MOVIES" />
+                <span>MOVIES</span>
+            </a>
+            <a>
+                <img src="/resources/images/series-icon.svg" alt="SERIES" />
+                <span>SERIES</span>
+            </a>
+        </NavMenu>
+        <Login onClick={handleAuth}>Login</Login>
     </Nav>
   )
 }
@@ -48,21 +80,83 @@ const Logo = styled.a`
 `;
 
 const NavMenu = styled.div`
-    display: flex;
-    flex-direction: row;
     align-items: center;
-    flex-flow: row nowrap;
-    height: 100%;
-    justify-content: flex-end;
-    margin: 0px;
-    padding: 0px;
-    position: relative;
-    margin-left: 25px;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      margin-right: 5px;
+      z-index: auto;
+    }
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
 
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
 
     // @media only screen and (max-width: 768px){
     //     display: none;
     // }
+`;
+
+const Login = styled.a`
+  background-color: rgba(0,0,0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  cursor: pointer;
+
+  &:hover{
+    background-color: white;
+    color: black;
+    border-color: transparent;
+  }
 `;
 
 export default header
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBI0RKFVHyufoNfaLPWLzEb6pd368ncUgI",
+    authDomain: "hotstar-clone-59df3.firebaseapp.com",
+    projectId: "hotstar-clone-59df3",
+    storageBucket: "hotstar-clone-59df3.appspot.com",
+    messagingSenderId: "11520669839",
+    appId: "1:11520669839:web:44dd2151ac672f99ae58fa",
+    measurementId: "G-HGGGCQ7HPX"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+
+export {auth, provider, storage};
+export default db;
