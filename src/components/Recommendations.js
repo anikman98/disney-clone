3f2d93a38c4dc58e84947e28f9041380274af1d0
anikman <anikman98@gmail.com>
2022-10-16 13:53:35 +0530

    Added Recommendation css and content

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -1,14 +1,31 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Recommendations = () => {
   return (
     <Container>
-        <h4>Recommendations for you</h4>
+        <h2>Recommended for you</h2>
         <Content>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
+            <Wrap>
+                <Link to='/'>
+                    <img src="https://i.ytimg.com/vi/0L0QinW_A70/mqdefault.jpg" alt="" />
+                </Link>
+            </Wrap>
+            <Wrap>
+                <Link to='/'>
+                    <img src="https://i.ytimg.com/vi/0L0QinW_A70/mqdefault.jpg" alt="" />
+                </Link>
+            </Wrap>
+            <Wrap>
+                <Link to='/'>
+                    <img src="https://i.ytimg.com/vi/0L0QinW_A70/mqdefault.jpg" alt="" />
+                </Link>
+            </Wrap>
+            <Wrap>
+                <Link to='/'>
+                    <img src="https://i.ytimg.com/vi/0L0QinW_A70/mqdefault.jpg" alt="" />
+                </Link>
+            </Wrap>
         </Content>
     </Container>
   )
@@ -22,11 +39,43 @@ const Container = styled.div`
 const Content = styled.div`
     display: grid;
     grid-gap: 25px;
+    gap: 25px;
     grid-template-columns: repeat(4, minmax(0, 1fr));
 
     @media only screen and (max-width: 768px){
         grid-template-columns: repeat(2, minimax(0, 1fr));
     }
+`;
+
+const Wrap = styled.div`
+    padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px, rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img{
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    transition: opacity 500ms ease-in-out 0s;
+    width: 100%;
+    z-index: 1;
+    top: 0;
+  }
+
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
 `;
 
 export default Recommendations;
